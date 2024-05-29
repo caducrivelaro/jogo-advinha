740efef4a392fb91547989b526e222666f90e171
carlos crivelaro <carloseduardocrivelaro2007@gmail.com>
2024-05-29 15:57:20 -0300

    código pronto

--- jogo.py
+++ jogo.py
@@ -5,4 +5,27 @@ def jogo_advinhação():
     tentativas = 0
 
 
-    
+print("Bem-vindo ao jogo da advinhação!")
+print("Estou pensando em um número entre 1 e 100")
+
+
+while True:
+    try:
+        palpite = int(input("Digite seu palpite"))
+        tentativas += 1
+
+        if palpite < numero_secreto:
+            print("Um pouco maior")
+
+        elif palpite > numero_secreto:
+            print("Um pouco maior")
+
+        else:
+             print("Parabens você acertou o número em {tentativas }tentativas")
+             break
+        
+    except ValueError:
+        print("Por favor, adicione um número válido")
+
+if __name__ == "__main__":
+    Jog0_advinhação()
